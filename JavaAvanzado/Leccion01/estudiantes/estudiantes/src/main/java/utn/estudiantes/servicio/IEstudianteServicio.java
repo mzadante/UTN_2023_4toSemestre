6b97b9b4ff6c08bdc093296f14a6816659b45dd2
@mzadante <mzadantemartinez@gmail.com>
2023-09-11 11:16:20 -0300

    java avanzado

---
+++ JavaAvanzado/Leccion01/estudiantes/estudiantes/src/main/java/utn/estudiantes/servicio/IEstudianteServicio.java
@@ -0,0 +1,9 @@
+package utn.estudiantes.servicio;
+
+import utn.estudiantes.modelo.Estudiante;
+
+import java.util.List;
+
+public interface IEstudianteServicio {
+    List<Estudiante> listarEstudiantes();
+}
