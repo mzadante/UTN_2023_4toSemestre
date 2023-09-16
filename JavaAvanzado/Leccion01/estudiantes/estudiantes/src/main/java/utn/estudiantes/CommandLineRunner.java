2b1385c87d96f0b23116ee239d520a4689f64be7
@mzadante <mzadantemartinez@gmail.com>
2023-09-16 11:08:30 -0300

    borrar

--- JavaAvanzado/Leccion01/estudiantes/estudiantes/src/main/java/utn/estudiantes/CommandLineRunner.java
+++
@@ -1,4 +0,0 @@
-package utn.estudiantes;
-
-public interface CommandLineRunner {
-}
--- JavaAvanzado/Leccion01/estudiantes/estudiantes/src/main/java/utn/estudiantes/servicio/IEstudianteServicio.java
+++
@@ -1,9 +0,0 @@
-package utn.estudiantes.servicio;
-
-import utn.estudiantes.modelo.Estudiante;
-
-import java.util.List;
-
-public interface IEstudianteServicio {
-    List<Estudiante> listarEstudiantes();
-}
