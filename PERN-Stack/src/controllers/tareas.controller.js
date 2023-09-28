fe0713bb59b7b435de94d86a101c950544eb9002
@mzadante <mzadantemartinez@gmail.com>
2023-09-28 00:53:43 -0300

    clase 6 javascript completa PERN parte 2
    Clase 06 Proyecto con stack pern por Daniel Guerrero -> Parte 2

--- PERN-Stack/src/controllers/tareas.controller.js
+++ PERN-Stack/src/controllers/tareas.controller.js
@@ -1,9 +1,58 @@
-export const listarTareas = (req, res) => res.send('obteniendo tareas');
+import {pool} from '../db.js';
 
-export const listarTarea = (req, res) => res.send('obteniendo tarea unica');
+export const listarTareas = async (req, res) => {
+    const resultado = await pool.query('SELECT * FROM tareas');
+    console.log(resultado);
+    return res.json(resultado.rows);
+}
 
-export const crearTarea = (req, res) => res.send('creando tarea');
+export const listarTarea = async (req, res) => {
+    const resultado = await pool.query('SELECT * FROM tareas WHERE id = $1', [req.params.id]);
+    if (resultado.rowCount === 0) {
+        return res.status(404).json({
+            message: 'No existe tarea con ese id'
+        });
+    }
+    return res.json(resultado.rows);
+}
 
-export const actualizarTarea = (req, res) => res.send('actualizando tarea unica por id');
+export const crearTarea = async (req, res, next) =>{
+    const {titulo, descripcion} = req.body;
 
-export const eliminarTarea = (req, res) => res.send('eliminando tarea unica');
+    try {
+        
+        const result = await pool.query('INSERT INTO tareas (titulo, descripcion) VALUES ($1, $2) RETURNING *', [titulo, descripcion]);
+        res.json(result.rows[0]);
+        console.log(result.rows[0]);
+    } catch (error) {
+        if(error.code === '23505') {
+            return res.status(409).json({
+                message: 'Ya existe una tarea con ese titulo'
+            });
+        }
+        console.log(error);
+        next(error);
+    }
+};
+export const actualizarTarea = async (req, res) => {
+    const {titulo, descripcion} = req.body;
+    const id = req.params.id;
+    const result = await pool.query('UPDATE tareas SET titulo = $1, descripcion = $2 WHERE id = $3 RETURNING *', [titulo, descripcion, id]);
+    if (result.rowCount === 0) {
+        return res.status(404).json({
+            message: 'No existe una tarea con ese id'
+        });
+    }
+    return res.json(result.rows[0]);
+}
+
+export const eliminarTarea = async (req, res) => {
+    const resultado = await pool.query('DELETE FROM tareas WHERE id = $1', [req.params.id]);
+    if (resultado.rowCount === 0) {
+        return res.status(404).json({
+            message: 'No existe una tarea con ese id'
+        });
+    }
+
+    return res.send(204);
+}
--- PERN-Stack/src/index.js
+++ PERN-Stack/src/index.js
@@ -1,4 +1,6 @@
 import app from "./app.js";
 
 app.listen(3000);
+
 console.log("Server on port", 3000);
+   
