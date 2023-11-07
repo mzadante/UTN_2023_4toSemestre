010f2847c2f46d4e85b288b57b8f45801a75a694
@mzadante <mzadantemartinez@gmail.com>
2023-11-07 11:34:14 -0300

    clase 11 python avanzado
    Trabajamos con matplotlib generando graficos de los paises de america del sur usando archivo csv

--- Python/python_avanzado/app/main.py
+++ Python/python_avanzado/app/main.py
@@ -11,13 +11,14 @@ def run():
   charts.generate_pie_chart(countries, percentages)
  
   country = input('Type Country => ')
+  print(country)
 
   result = utils.population_by_country(data, country)
 
   if len(result) > 0:
     country = result[0]
     labels, values = utils.get_population(country)
-    charts.generate_bar_chart(labels, values)
+    charts.generate_bar_chart(country['country'], labels, values)
 
 if __name__ == '__main__':
   run()
